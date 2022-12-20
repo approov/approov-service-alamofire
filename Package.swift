bcea423e945e1c60030633ce2d6045a14ae6f7f3
ivol <ivol@criticalblue.com>
2022-12-20 13:35:05 +0000

    3.1.0 SDK bitcode

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let releaseTAG = "3.0.0"
+let releaseTAG = "3.1.0"
 let package = Package(
     name: "ApproovSession",
     platforms: [.iOS(.v10)],
@@ -27,7 +27,7 @@ let package = Package(
         .binaryTarget(
             name: "Approov",
             url: "https://github.com/approov/approov-ios-sdk-bitcode/releases/download/" + releaseTAG + "/Approov.xcframework.zip",
-            checksum : "ee9e432bca8060f109d00c04bbd28766dda6a38612d5056d737f54000e01f311"
+            checksum : "f708722e2aa25102d4b92c97959e4c0d7abaa934d0413dd4e5f7a04c04c0410f"
         )
     ]
 )
