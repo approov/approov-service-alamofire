4382675973cb48aeef122a687847747279932012
ivo liondov <ivoliondov@Sanctuary.local>
2026-03-16 09:20:58 +0000

    Add swift pm changes to Approov binary dependency; increase package version

--- Package.swift
+++ Package.swift
@@ -2,12 +2,12 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 // Release tag
-let releaseTAG = "3.5.4"
+let releaseTAG = "3.5.5"
 // SDK package version (used for both iOS and watchOS)
 let sdkVersion = "3.5.3"
 
 let package = Package(
-    name: "ApproovSession",
+    name: "ApproovAFSession",
     platforms: [
         .iOS(.v11),
         .watchOS(.v9)
@@ -15,34 +15,30 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ApproovSession",
-            targets: ["ApproovSession"]
+            name: "ApproovAFSession",
+            targets: ["ApproovAFSession"]
         ),
-        .library(name: "ApproovSessionDynamic", type: .dynamic, targets: ["ApproovSession"])
+        .library(name: "ApproovAFSessionDynamic", type: .dynamic, targets: ["ApproovAFSession"])
     ],
     dependencies: [
         // Package's external dependencies and from where they can be fetched:
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/apple/swift-http-structured-headers.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-http-structured-headers.git", from: "1.0.0"),
+        // Force-unwrapping is safe here because sdkVersion is a valid semantic version string
+        .package(url: "https://github.com/approov/approov-ios-sdk.git", from: Version(sdkVersion)!)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ApproovSession",
+            name: "ApproovAFSession",
             dependencies: [
-                "Approov",
-                "Alamofire",
+                .product(name: "Approov", package: "approov-ios-sdk"),
+                .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "RawStructuredFieldValues", package: "swift-http-structured-headers")
             ],
             path: "Sources/ApproovSession",  // Point to the shared source code
             exclude: ["README.md", "LICENSE"]
-        ),
-        // Binary target for the merged xcframework
-        .binaryTarget(
-            name: "Approov",
-            url: "https://github.com/approov/approov-ios-sdk/releases/download/\(sdkVersion)/Approov.xcframework.zip",
-            checksum: "acfbee9e6c8009535c070c87c0d5756c2ba8f78e7b8147d7632f12bfcb940c89" // SHA256 checksum of the xcframework zip file
         )
     ]
 )
